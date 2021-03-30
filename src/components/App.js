c43fc0323d4a096e3b206ac4469b49ee9564256d
TheMike12 <gibsonfan95@yahoo.com>
2021-03-29 22:00:52 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Background from "./Background";
+import '../styles/index.css';
+
+const App = () => {
+  
+  return (
+    <div className="ooo">
+      hi   
+      <Background />  
+    </div>
+  )
+
+}
+
+export default App
---
+++ src/components/Background.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/Backgroundd.css';
+
+const Background = () => {
+  
+  return (
+      <div className="ooo">
+        <div className="background-1"></div>
+      </div>
+  )
+
+}
+
+export default Background
